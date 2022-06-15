d93c89559b80f59834373dcf1d15c2a9610ba88d
Paulo Baronceli <hello@novafinance.me>
2022-06-15 21:34:59 +1200

    fix build script

--- build-novanetwork.sh
+++ build-novanetwork.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build Nova Network using Golang.
 echo "Building Nova Network..."
-make novanetwork
+make novanetwork-full
 
 # Rename 'geth' to 'novanetwork' inside the 'build/bin' folder.
 sudo mv build/bin/geth build/bin/novanetwork
