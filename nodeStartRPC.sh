14cb2f801a3ac34a0a378764c6327796a7867a34
Nova Network Incl <hello@novafinance.me>
2022-06-22 14:32:38 +1200

    update bootnodes

--- nodeStartRPC.sh
+++ nodeStartRPC.sh
@@ -5,7 +5,12 @@
 # This script requires elevated access privileged (CHMOD 777), please run:
 # chmod 777 startNode.sh before launching, and then bash start-node.sh to start.
 
-# Welcome message.
+#Bootnode Parameters
+publicNodeKey=8a4d05d13ccf1e5b8433633b96987d6293f22a92db2bb04c10fc2e490ba9edf33d3148eb2b355e57f13bd4dc443f00f9d95b7c790dbf92cbe5564eacb20b5789
+publicNodeIp=87.106.153.207
+publicNodePort=30303
+
+# Welcome Message
 echo "Welcome to Nova Network!"
 echo ""
 echo "Starting 1/2 - Configuring the network genesis block..."
@@ -14,4 +19,4 @@ echo ""
 
 # Starting Nova Network
 echo "Starting 2/2 - Starting your Nova Network RPC node..."
-novanetwork --datadir .dag --networkid "87" --rpc.allow-unprotected-txs --nodiscover --maxpeers "1" --syncmode "full" --ipcdisable --bootnodes "" --graphql --http.port "8545" --http --http.addr "0.0.0.0" --http.corsdomain "*" --http.api "eth,net,web3"  --ws --ws.addr "0.0.0.0" --ws.origins "*" --ws.api "eth,net,web3"
+novanetwork --datadir .dag --networkid "87" --rpc.allow-unprotected-txs --nodiscover --maxpeers "1" --syncmode "full" --ipcdisable --bootnodes "enode://${publicNodeKey}@${publicNodeIp}:${publicNodePort}?discport=0" --graphql --http.port "8545" --http --http.addr "0.0.0.0" --http.corsdomain "*" --http.api "eth,net,web3"  --ws --ws.addr "0.0.0.0" --ws.origins "*" --ws.api "eth,net,web3" --miner.etherbase "0x000000000000000000000000000000000000dEaD"
